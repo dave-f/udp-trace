db99ed5a2344a2d75a324948172fbdd4d94859f8
Dave Footitt <dave@bogus.net>
2013-04-23 11:15:53 +0100

    Settings added

--- udptrace.py
+++ udptrace.py
@@ -5,19 +5,18 @@ import threading
 import datetime
 import functools
 
-UDP_IP 		= "127.0.0.1"
-UDP_PORT 	= 1777
-MAX_ENTRIES = 100
-
-quitEvent 	= threading.Event()
+quitEvent = threading.Event()
 
 #
 # Worker thread
 class UdpTraceThread(threading.Thread):
 	"""Simple thread to read data from a UDP port"""
-	def __init__(self, theView):
+	def __init__(self, theView, ipAddress, portNo, maxEntries):
 		threading.Thread.__init__(self)
 		self.theView = theView
+		self.ipAddress = ipAddress
+		self.portNo = portNo
+		self.maxEntries = maxEntries
 
 	def run(self):
 		now = datetime.datetime.now()
@@ -25,7 +24,7 @@ class UdpTraceThread(threading.Thread):
 		try:
 			sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 			sock.settimeout(1.0)
-			sock.bind((UDP_IP,UDP_PORT))
+			sock.bind((self.ipAddress,self.portNo))
 			while (not quitEvent.is_set()):
 				try:
 					data,addr = sock.recvfrom(1024)
@@ -41,7 +40,7 @@ class UdpTraceThread(threading.Thread):
 	def update(self, data):
 		lines, _ = self.theView.rowcol(self.theView.size())
 		e = self.theView.begin_edit()
-		if ( lines >= MAX_ENTRIES ):
+		if ( lines >= self.maxEntries ):
 			# Erase beginning
 			pt = self.theView.text_point(0, 0)
 			self.theView.erase(e,self.theView.full_line(pt))
@@ -54,25 +53,32 @@ class UdpTraceThread(threading.Thread):
 class UdpTraceCommand(sublime_plugin.WindowCommand):
 	"""Prints out data received over the network into a Sublime buffer"""
 	def run(self):
+		# Load our settings
+		s = sublime.load_settings("udptrace.sublime-settings")
+		ipAddress = s.get("address","127.0.0.1")
+		portNo = s.get("port","1777")
+		bufferMode = s.get("buffer_mode","Packages/Text/Plain text.tmLanguage")
+		maxEntries = s.get("max_entries")
 		ok = True
 		for v in self.window.views():
-			if (v.name() == "* UDP Trace*"):
+			# Don't run if we are already
+			if (v.name() == "*UDP Trace*"):
 				ok = False
 				break
 
 		if (ok):
 			v = self.window.new_file()
-			v.set_name("* UDP Trace*")
+			v.set_name("*UDP Trace*")
 			v.set_scratch(True)
-			v.set_syntax_file('Packages/XML/XML.tmLanguage')
+			v.set_syntax_file(bufferMode)
 			quitEvent.clear()
-			t = UdpTraceThread(v)
+			t = UdpTraceThread(v, ipAddress, portNo, maxEntries)
 			t.start()
 		else:
 			sublime.error_message("Already active")
 
 class CloseListener(sublime_plugin.EventListener):
 	def on_close(self, v):
-		if (v.name() == "* UDP Trace*"):
+		if (v.name() == "*UDP Trace*"):
 			# Signal to thread
 			quitEvent.set()
